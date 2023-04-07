da2cc463b7af8f8014d348a8e4af030675df65d3
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-04-07 21:15:51 +0200

    UI

---
+++ assets/JS/script.js
@@ -0,0 +1,27 @@
+let darkModeBtn = document.querySelector("#check-5");
+let section = document.querySelector("section");
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
+
+const savedMode = localStorage.getItem("Mode");
+if(savedMode !== undefined || savedMode !== null){
+  darkModeBtn.checked = savedMode === 'light';
+}
+
+
+addEventListener('change', function(){
+  let value = darkModeBtn.checked ? 'light' : 'dark';
+
+  if (darkModeBtn.checked){
+    console.log("light");
+    body.classList.add('light-mode');
+    body.classList.remove('dark-mode');
+  }
+  else {
+    console.log("dark");
+    body.classList.remove("light-mode");
+    body.classList.add("dark-mode");
+  }
+
+  localStorage.setItem("Mode", value)
+})
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/CSS/style.css">
+    <link rel="stylesheet" href="/assets/CSS/darkMode.css">
+    <link rel="stylesheet" href="/assets/CSS/lightMode.css">
+
+</head>
+<body>
+<div class="container">
+    
+    <div class="left-col" >
+        <nav style="display: flex; flex-direction: column; align-items: center; justify-content: space-between;">
+            <img src="https://i.postimg.cc/JzKwwG16/arrow.png" class="back-btn">
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">My Stats</a></li>
+                <li><a href="#">Members</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Help</a></li>
+            </ul>
+            <div class="checkbox-wrapper-5" >
+                <div class="check">
+                    <input type="checkbox" id="check-5">
+                    <label for="check-5"></label>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <section>
+        <header>
+            <svg  class="menu-btn" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M20 5V2H0V5H20ZM20 11V8H0V11H20ZM20 17V14H0V17H20Z" fill="white"/>
+</svg>
+            <p class="logo">Logo</p>
+        </header>
+        <div class="cv">
+            <div class="content">
+                <h1>Data Analytics</h1>
+                <p>Making sense of your traffic.</p>
+
+                <a href="#" class="cta">Go to my stats</a>
+            </div>
+        </div>
+    </section>
+
+</div>
+<script src="/assets/JS/script.js"></script>
+</body>
+</html>

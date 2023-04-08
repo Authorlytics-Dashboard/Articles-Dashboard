02acc505d5a174ae64e64efd819bd10f71a87b7d
Nada Saeed <nada1998saeed@gmail.com>
2023-04-08 20:41:49 +0200

    adding login page

---
+++ views/dashboard/home.php
@@ -0,0 +1,15 @@
+<section>
+    <header>
+        <svg  class="menu-btn" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 5V2H0V5H20ZM20 11V8H0V11H20ZM20 17V14H0V17H20Z" fill="white"/>
+        </svg>
+        <p class="logo">Logo</p>
+    </header>
+    <div class="cv">
+        <div class="content">
+            <h1>Data Analytics</h1>
+            <p>Making sense of your traffic.</p>
+            <a href="#" class="cta">Go to my stats</a>
+        </div>
+    </div>
+</section>

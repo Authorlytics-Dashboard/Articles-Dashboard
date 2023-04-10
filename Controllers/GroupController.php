61cdcce6f19f48c7c5cc456bbc995f0a2c1c3a54
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-04-11 00:26:44 +0200

    MVC file Structure

---
+++ Controllers/GroupController.php
@@ -0,0 +1,25 @@
+<?php
+
+class GroupController implements CrudInterface {
+    public function create($data){
+
+    }
+    public function show($id){
+
+    }
+    public function showAll(){
+
+    }
+    public function update($id, $data){
+
+    }
+    public function delete($id){
+        
+    }
+}
+
+
+
+
+
+?>

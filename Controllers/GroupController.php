1513348f50ca5d7f1074a4097c40c4afbe60b956
radwa nabil <radwaismail444@gmail.com>
2023-04-11 13:25:08 +0200

    modified group controller

--- Controllers/GroupController.php
+++ Controllers/GroupController.php
@@ -2,7 +2,15 @@
 
 class GroupController implements CrudInterface {
     public function create($data){
-
+        // if(isset($_POST['action']) && $_POST['action'] == 'Create') {
+        //     $name = $_POST['name'];
+        //     $description = $_POST['description'];
+        //     $avatar = $_POST['avatar'];
+            
+        //     $sql = "INSERT INTO groups (name, description, avatar) VALUES ('$name', '$description', '$avatar')";
+            
+        // }
+        
     }
     public function show($id){
 
--- group.php
+++ group.php
@@ -6,7 +6,7 @@
 
 <section>
     <div class="container py-4 border my-5 mx-auto">
-        <form method="post" action="index.php" class="w-75 mx-auto">
+        <form method="post" action="./Controllers/GroupController.php" class="w-75 mx-auto">
             <div class=" mb-3">
                 <label for="name" class="form-label">Group Name</label>
                 <input type="text" class="form-control" name="name" id="name">

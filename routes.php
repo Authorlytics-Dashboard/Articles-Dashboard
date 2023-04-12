f954fb41b1c587887ea66a95d8d8eda47cc2249b
marim99 <marim.khaled99@gmail.com>
2023-04-12 16:18:14 +0200

    finish show all groups

--- routes.php
+++ routes.php
@@ -4,7 +4,9 @@
     if($uri[1] == "home") {
         include_once("./views/dashboard/home.php");
     }elseif($uri[1] == "groups") {
-        include_once("./views/dashboard/groups/groups.php");
+        include_once("./views/dashboard/groups/index.php");
+    }elseif($uri[1] == "CreateGroup") {
+        include_once("./views/dashboard/groups/group.php");
     }elseif($uri[1] == "users") {
         include_once("./views/dashboard/users/user.php");
     }elseif($uri[1] == "articles") {

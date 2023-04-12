9c16459b508afbf6d0a6446ddfa0d8f69b65f85a
Nada Saeed <nada1998saeed@gmail.com>
2023-04-12 07:56:28 +0200

    updating dashboard views

--- views/dashboard/groups/group.php
+++ views/dashboard/groups/group.php
@@ -1,11 +1,3 @@
-
-
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
-</head>
-
 <section>
     <div class="container py-4 border my-5 mx-auto">
         <form method="post" action="index.php" class="w-75 mx-auto">

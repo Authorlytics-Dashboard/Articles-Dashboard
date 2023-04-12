2388b506024d80c6e85757a8329a212dae677e66
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-04-12 22:03:53 +0200

    delete, show group
    Co-Authored-By: ‪Mariam Khaled ‬‏ <78925756+Marim99@users.noreply.github.com>
    Co-Authored-By: Nada Saeed <77510429+Nada98Sakr@users.noreply.github.com>
    Co-Authored-By: Hager Khaled  <78926069+hagerk720@users.noreply.github.com>
    Co-Authored-By: Radwa nabil <65567343+radwanabil@users.noreply.github.com>

---
+++ views/dashboard/groups/show.php
@@ -0,0 +1,13 @@
+<?php     require_once('Classes/Group.php'); 
+    $groupId = $_GET['id'];
+    // get data of group by its id and show it in fields 
+    $group = new Group();
+    $g = $group ->showGroupByID($groupId);
+    $g = $g[0];
+?>
+
+<section class="groupSection">
+    <h5><?php echo $g["gname"]; ?></h5>
+    <h5><?php echo $g["description"]; ?></h5>
+    <img src="../../../assets/Images/<?php echo $g['avatar'] ?>" alt="">
+</section>

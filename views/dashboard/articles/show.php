245edf45a886ddea665ebe58baf7ffce2efc5ddc
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-04-18 00:15:37 +0200

    Article Validation

--- views/dashboard/articles/show.php
+++ views/dashboard/articles/show.php
@@ -43,12 +43,6 @@ $likeCount =$row['num_likes'];
 
 $likes = array();
 
-// get creator name
-// $sql = "SELECT users.uname FROM articles INNER JOIN users ON articles.uid = users.uid;";
-// $result = mysqli_query($conn, $sql);
-// $row = mysqli_fetch_assoc($result);
-
-
 // Close the database connection
 mysqli_close($conn);
 ?>

f2a6c4454884e8b0a2d0bc7ca96ff9b6111570b3
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-04-17 21:03:30 +0200

    Article card fix

--- views/dashboard/articles/show.php
+++ views/dashboard/articles/show.php
@@ -55,176 +55,158 @@ mysqli_close($conn);
 
 
 <style>
-    
 
-.container {
-  padding: 20px;
-}
+  .container {
+    padding: 20px;
+  }
 
-.profile-card {
-	background-color: #222222;
-  margin-bottom: 20px;
-			
-		}
+  .profile-card {
+    background-color: #222222;
+    margin-bottom: 20px;
+  }
+      
+  .profile-pic {
+    border-radius: 50%;
+    position: absolute;
+    top: -65px;
+    left: 0;
+    right: 0;
+    margin: auto;
+    z-index: 1;
+    max-width: 100px;
+    transition: all 0.4s;
+  }
+
+  .profile-info {
+    color: #BDBDBD;
+    padding: 25px;
+    position: relative;
+    margin-top: 15px;
+  }
 		
-.profile-pic {
-  border-radius: 50%;
-  position: absolute;
-  top: -65px;
-  left: 0;
-  right: 0;
-  margin: auto;
-  z-index: 1;
-  max-width: 100px;
-  -webkit-transition: all 0.4s;
-		  transition: all 0.4s;
-				}
-
-				
-.profile-info {
-		color: #BDBDBD;
-		padding: 25px;
-	    position: relative;
-	    margin-top: 15px;
-				}
-		
-.profile-info h2 {
-	color: #E8E8E8;
+  .profile-info h2 {
+    color: #E8E8E8;
     letter-spacing: 4px;
-	  padding-bottom: 12px;
-				}
-				
-.profile-info span {
-	display: block;
+    padding-bottom: 12px;
+  }
+
+  .profile-info span {
+    display: block;
     font-size: 12px;
     color: #4CB493;
-	letter-spacing: 2px;
-			}
-
-.profile-info a {
-	 color: #4CB493;
-		}
-.profile-info i {
-	    padding: 15px 35px 0px 35px;
-		}
-		
-
-.profile-card:hover .profile-pic {
-	transform: scale(1.1);
-		}
-
-.profile-card:hover .profile-info hr  {
-	opacity: 1;
-		}
-		
-		
-		
-		
-/* Underline From Center */
-.hvr-underline-from-center {
-  display: inline-block;
-  vertical-align: middle;
-  -webkit-transform: translateZ(0);
-  transform: translateZ(0);
-  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-  -moz-osx-font-smoothing: grayscale;
-  position: relative;
-  overflow: hidden;
-}
-.hvr-underline-from-center:before {
-  content: "";
-  position: absolute;
-  z-index: -1;
-  left: 52%;
-  right: 52%;
-  bottom: 0;
-  background: #FFFFFF;
-  border-radius: 50%;
-  height: 3px;
-  -webkit-transition-property: all;
-  transition-property: all;
-  -webkit-transition-duration: 0.2s;
-  transition-duration: 0.2s;
-  -webkit-transition-timing-function: ease-out;
-  transition-timing-function: ease-out;
-}
-.profile-card:hover .hvr-underline-from-center:before, .profile-card:focus .hvr-underline-from-center:before, .profile-card:active .hvr-underline-from-center:before {
-  left: 0;
-  right: 0;
-  height: 1px;
-  background: #CECECE;
-  border-radius: 0;
-}
-
-/* likes button */
-.LikesContainer input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-  height: 0;
-  width: 0;
-}
-
-.LikesContainer {
-  display: block;
-  position: relative;
-  cursor: pointer;
-  font-size: 20px;
-  user-select: none;
-  transition: 100ms;
-}
-
-.checkmark {
-  top: 0;
-  left: 0;
-  height: 2em;
-  width: 2em;
-  transition: 100ms;
-  animation: dislike_effect 400ms ease;
-}
-
-.LikesContainer input:checked ~ .checkmark path {
-  fill: #FF5353;
-  stroke-width: 0;
-}
-
-.LikesContainer input:checked ~ .checkmark {
-  animation: like_effect 400ms ease;
-}
-
-/* .LikesContainer:hover {
-  transform: scale(1);
-} */
-
-@keyframes like_effect {
-  0% {
-    transform: scale(0);
+    letter-spacing: 2px;
   }
 
-  50% {
-    transform: scale(1.2);
+  .img-responsive{
+    width:100%; 
+    height: 330px;
+  }
+  .profile-info a {
+    color: #4CB493;
+  }
+  .profile-info i {
+    padding: 15px 35px 0px 35px;
   }
 
-  100% {
-    transform: scale(1);
+  .artcilesCard{
+    width: 60em !important;
   }
-}
-
-@keyframes dislike_effect {
-  0% {
-    transform: scale(0);
+  .profile-card:hover .profile-pic {
+    transform: scale(1.1);
   }
 
-  50% {
-    transform: scale(1.2);
+  .profile-card:hover .profile-info hr {
+    opacity: 1;
   }
 
-  100% {
-    transform: scale(1);
+  /* Underline From Center */
+  .hvr-underline-from-center {
+    display: inline-block;
+    vertical-align: middle;
+    transform: translateZ(0);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    backface-visibility: hidden;
+    -moz-osx-font-smoothing: grayscale;
+    position: relative;
+    overflow: hidden;
   }
-}
+
+  .hvr-underline-from-center:before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    left: 52%;
+    right: 52%;
+    bottom: 0;
+    background: #FFFFFF;
+    border-radius: 50%;
+    height: 3px;
+    transition-property: all;
+    transition-duration: 0.2s;
+    transition-timing-function: ease-out;
+  }
+
+  .profile-card:hover .hvr-underline-from-center:before,
+  .profile-card:focus .hvr-underline-from-center:before, 
+  .profile-card:active .hvr-underline-from-center:before {
+    left: 0;
+    right: 0;
+    height: 1px;
+    background: #CECECE;
+    border-radius: 0;
+  }
+
+  /* likes button */
+  .LikesContainer input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .LikesContainer {
+    display: block;
+    position: relative;
+    cursor: pointer;
+    font-size: 20px;
+    user-select: none;
+    transition: 100ms;
+  }
+
+  .checkmark {
+    top: 0;
+    left: 0;
+    height: 2em;
+    width: 2em;
+    transition: 100ms;
+    animation: dislike_effect 400ms ease;
+  }
+
+  .LikesContainer input:checked ~ .checkmark path {
+    fill: #FF5353;
+    stroke-width: 0;
+  }
+
+  .LikesContainer input:checked ~ .checkmark {
+    animation: like_effect 400ms ease;
+  }
+
+  .likesCount{
+    transform: translate(-380px, -30px);
+  }
+
+  @keyframes like_effect {
+    0% {transform: scale(0);}
+    50% {transform: scale(1.2);}
+    100% {transform: scale(1);}
+  }
+
+  @keyframes dislike_effect {
+    0% {transform: scale(0);}
+    50% {transform: scale(1.2);}
+    100% {transform: scale(1);}
+  }
 
 </style>
 
@@ -235,9 +217,9 @@ mysqli_close($conn);
   <div class="row ">
     <div class="col-lg-12">
       <div>
-        <div class="col-md-12" style="width: 740px;">
+        <div class="col-md-12 artcilesCard">
           <div class="profile-card text-center">
-            <img class="img-responsive" style="width:100%; height: 270px;" src="../../../assets/Images/<?php echo $article['photo'] ?>" alt="">
+            <img class="img-responsive" src="../../../assets/Images/<?php echo $article['photo'] ?>" alt="">
             <div class="profile-info">
                 <h2 class="hvr-underline-from-center"><?php echo $article["title"]; ?></h2>
                 <div><?php echo $article["body"]; ?></div>
@@ -261,7 +243,7 @@ mysqli_close($conn);
                     </div>
                   </label>
                 </form>
-                <span style="transform: translate(-270px, -30px);" ><?php echo $likeCount . ' likes'; ?></span> 
+                <span class="likesCount" ><?php echo $likeCount . ' likes'; ?></span> 
               </div>
           </div>
         </div>

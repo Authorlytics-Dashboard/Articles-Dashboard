aeda438b0988aeff8770e9083e3121e409dfba18
Nada Saeed <nada1998saeed@gmail.com>
2023-04-18 07:01:02 +0200

    adding user validation

--- Classes/UserValidator.php
+++ Classes/UserValidator.php
@@ -1,25 +1,88 @@
 <?php
 class UserValidator {
-    public function validateUserName($name) {
-        if (empty($name)) {
+    public function __construct(private $data){
+    }
+
+    public function validateUserName() {
+        if (empty($this->data['uname'])) {
             return "Name is required";
-        } elseif (strlen($name) < 3) {
+        } elseif (strlen($this->data['uname']) < 3) {
             return "Name should be at least 3 characters long";
         } else {
             return null; 
         }
     }
     
-    public function validateUserEmail($email) {
-        if (empty($email)) {
-            return "email is required";
-        } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $emailErr = "Invalid email format";
+    public function validateUserEmail() {
+        if (empty($this->data['email'])) {
+            return "Email is required";
+        } elseif(!filter_var($this->data['email'], FILTER_VALIDATE_EMAIL)){
+            return "Invalid email format";
         } else {
             return null; 
         }
     }
 
+    public function validateUserPassword() {
+        if (empty($this->data['password'])) {
+            return "password is required";
+        } elseif (strlen($this->data['password']) < 6) {
+            return "password should be at least 6 characters long";
+        } else {
+            return null; 
+        }
+    }
+    
+    public function validateUserMobile() {
+        if (empty($this->data['mobile'])) {
+            return "Mobile Number is required";
+        } elseif (!preg_match('/^01[0-9]{9}$/', $this->data['mobile'])) {
+            return "mobile should only contain letters";
+        } elseif (strlen($this->data['mobile']) < 11) {
+            return "mobile should be at least 3 characters long";
+        } else {
+            return null; 
+        }
+    }
+
+    public function validateUserGroup(){
+        $groups = new User('groups', "GroupsErrors.log",'id');
+        $group = $groups->search(array("column" => "gid", "value" => $this->data['gid']));
+        if(!$group){
+            return "Selected group does not exist";
+        } else{
+            return null;
+        }
+    }
+    
+    public function validateUserAvatar() {
+        $allowedExtensions = ['png', 'jpeg', 'jpg', 'gif'];
+        $extension = strtolower(pathinfo($this->data['avatar'], PATHINFO_EXTENSION));
+        if (empty($this->data['avatar'])) {
+            return "Avatar is required";
+        } elseif (!in_array($extension, $allowedExtensions)) {
+            return "Avatar should be png, jpeg, jpg or gif";
+        } else {
+            return null; 
+        }
+    }
+
+    public function isValid(){
+        if( $this->validateUserName() && $this->validateUserEmail() 
+            && $this->validateUserPassword() && $this->validateUserMobile()
+            && $this->validateUserGroup() && $this->validateUserAvatar() ){
+            return null;
+        }else{
+            return [
+                'nameErr' => $this->validateUserName(), 
+                'emailErr' => $this->validateUserEmail(), 
+                'passwordErr' => $this->validateUserPassword(),
+                'mobileErr' => $this->validateUserMobile(), 
+                'groupErr' => $this->validateUserGroup(),
+                'avatarErr' => $this->validateUserAvatar()
+            ];
+        }
+    }
 }
 
 ?>
--- views/dashboard/users/create.php
+++ views/dashboard/users/create.php
@@ -1,14 +1,32 @@
+<?php
+    $error = array();
+    if (isset($_SESSION['error'])) {
+        $error = $_SESSION['error'];
+        unset($_SESSION['error']);
+        $data = $_SESSION['data'];
+        unset($_SESSION['data']);
+    }
+?>
+
 <section class="userSection">
     <div class="container py-4 border my-5 mx-auto">
         <form method="post" action="" class="w-75 mx-auto" enctype="multipart/form-data">
             <div class=" mb-3">
                 <label for="name" class="form-label">Username</label>
-                <input type="text" class="form-control" name="name" id="name">
+                <input type="text" class="form-control" name="name" id="name" value="<?= htmlspecialchars($data['uname'] ?? '') ?>">
+                <?php if( isset($error['nameErr']) ){ ?>
+                    <p class="col-12 text-danger"><?= $error['nameErr'] ?></p>
+                <?php } ?>
             </div>
+
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email" id="email">
+                <input type="email" class="form-control" name="email" id="email" value="<?= htmlspecialchars($data['email'] ?? '') ?>">
+                <?php if( isset($error['emailErr']) ){ ?>
+                    <p class="col-12 text-danger"><?= $error['emailErr'] ?></p>
+                <?php } ?>
             </div>
+
             <div class="mb-3">
                 <label for="group_id" class="form-label">Group</label>
                 <select class="form-control" name="group_id" id="group_id">
@@ -19,24 +37,43 @@
                         foreach ($groups as $g) {
                             $gid = $g['gid'];
                             $gname = $g['gname'];
-                            echo "<option value=\"$gid\">$gname</option>";
+                            echo "<option value=\"$gid\"";
+                            if(isset($data)){
+                                if ($data['gid'] == $gid) {
+                                    echo " selected";
+                                }
+                            }
+                            echo ">$gname</option>";
                         }
                     ?>
                 </select>
+                <?php if( isset($error['groupErr']) ){ ?>
+                    <p class="col-12 text-danger"><?= $error['groupErr'] ?></p>
+                <?php } ?>
             </div>
-
 
             <div class=" mb-3">
                 <label for="mobile" class="form-label">Mobile Number</label>
-                <input type="text" class="form-control" name="mobile" id="mobile">
+                <input type="text" class="form-control" name="mobile" id="mobile" value="<?= htmlspecialchars($data['mobile'] ?? '') ?>">
+                <?php if( isset($error['mobileErr']) ){ ?>
+                    <p class="col-12 text-danger"><?= $error['mobileErr'] ?></p>
+                <?php } ?>
             </div>
+
             <div class=" mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password" id="password">
+                <input type="password" class="form-control" name="password" id="password" value="<?= htmlspecialchars($data['password'] ?? '') ?>">
+                <?php if( isset($error['passwordErr']) ){ ?>
+                    <p class="col-12 text-danger"><?= $error['passwordErr'] ?></p>
+                <?php } ?>
             </div>
+
             <div class="mb-3">
                 <label for="avatar" class="form-label">Avatar</label>
                 <input type="file" class="form-control" name="avatar" id="avatar">
+                <?php if( isset($error['avatarErr']) ){ ?>
+                    <p class="col-12 text-danger"><?= $error['avatarErr'] ?></p>
+                <?php } ?>
             </div>
 
             <div class="mb-3 text-center mt-5 d-flex justify-content-end">
@@ -58,6 +95,18 @@
             'password' => $_POST['password'],
             'avatar' => $_FILES['avatar']['name']
         ];
-        $user->create($data);
+
+        $userValidation = new UserValidator($data);
+        if($userValidation->isValid() !== null){
+            $error = $userValidation->isValid();
+            session_start();
+            $_SESSION['data'] = $data;
+            $_SESSION['error'] = $error;
+            header("Location: /users/create");
+            exit();
+        } else {
+            // $user->create($data);
+            var_dump("Data Valid");
+        }
     }
 ?>

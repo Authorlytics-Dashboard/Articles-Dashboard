9b88e0632b97242a565c67c1d022d298c840d91d
marim99 <marim.khaled99@gmail.com>
2023-04-19 02:18:09 +0200

    unlike

--- views/dashboard/articles/show.php
+++ views/dashboard/articles/show.php
@@ -2,7 +2,7 @@
     $articleId = $_GET['id'];
     $articles = new Article('articles','ArticlesErrors.log','aid');
     $article = $articles->getRecordByID($articleId)[0];
-    // $articles->likeArticle($articleId);
+    $articles->likeArticle($articleId);
     $likeCount=$articles->displayLikes($articleId);
     ob_start();
 ?>
@@ -35,7 +35,7 @@
                       <div class="checkmark">
                         <svg viewBox="0 0 256 256">
                         <rect fill="none" height="256" width="256"></rect>
-                        <path d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z" stroke-width="20px" stroke="#FFF" fill="none"></path></svg>
+                        <path d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z" stroke-width="20px" stroke="#FFF" fill="<?php if($articles->hasUserLikedArticle($articleId, $articles->_auth->getUserId())) { echo '#FF5353'; } else { echo 'none'; } ?>"></path></svg>
                       </div>
                     </label>
                   </form>

d7d3ddd62022bea31b906d3d1d7b5880bf4daf24
marim99 <marim.khaled99@gmail.com>
2023-04-20 01:25:34 +0200

    modify create function

--- Classes/Article.php
+++ Classes/Article.php
@@ -1,79 +1,84 @@
 <?php
 
 class Article extends CRUD {
-    public function create($data){
-        try {
-            $this->connect();
-            $validator = new ArticleValidator();
-             $gname = $data['title'];
-            $nameError = $validator->validateArticleName($gname);
-            if ($nameError) {
-                $this->showError('name-error', $nameError);
-                return false;
-            }  
+    public function create($data) {
+      try {
+          $this->connect();
+  
+          $validator = new ArticleValidator();
+          $validData = $this->validateArticleData($data, $validator);
+          if (!$validData) {
+              return false;
+          }
+  
+          $photo = $this->uploadPhoto($_FILES['photo']);
+          $validData['photo'] = $photo;
+  
+          $this->save($validData);
+  
+          return true;
+      } catch (Exception $e) {
+          new Log($this->log_file, $e->getMessage());
+          return false;
+      }
+  }
+  
+  private function validateArticleData($data, $validator) {
+      $titleError = $validator->validateArticleName($data['title']);
+      if ($titleError) {
+          $this->showError('name-error', $titleError);
+          return false;
+      }
+  
+      $bodyError = $validator->validateArticleDescription($data['body']);
+      if ($bodyError) {
+          $this->showError('description-error', $bodyError);
+          return false;
+      }
+  
+      $photoError = $validator->validateArticleAvatar($data['photo']);
+      if ($photoError) {
+          $this->showError('avatar-error', $photoError);
+          return false;
+      }
+  
+      $validData = [
+          'title' => $data['title'],
+          'body' => $data['body'],
+          'post_date' => $data['post_date'] ? $data['post_date'] : date('Y-m-d H:i:s'),
+          'uid' => $data['uid']
+      ];
+  
+      return $validData;
+  }
+  
+  private function uploadPhoto($file) {
+      $target_file = "../assets/Images/" . basename($file["name"]);
+      move_uploaded_file($file["tmp_name"], __DIR__ . '/' . $target_file);
+  
+      return basename($file["name"]);
+  }
+    private function save($data) {
+      $sql = $this->buildInsertStatement($this->table, $data);
+  
+      if (mysqli_query($this->_dbHandler, $sql)) {
+          $id = mysqli_insert_id($this->_dbHandler);
+          $this->disconnect();
+          header('Location:/articles');
+          return $id;
+      } else {
+          $this->disconnect();
+          ob_flush();
+          header('location:/articles');
+          return false;
+      }
+  }
 
-            $description = $data['body'];
-            $descriptionError = $validator->validateArticleDescription($description);
-            if($descriptionError){
-                $this->showError('description-error', $descriptionError);
-                return false;
-            } 
-             
-            $avatar = $data['photo'];
-            $avatarError = $validator->validateArticleAvatar($avatar);
-            if($avatarError){
-                $this->showError('avatar-error', $avatarError);
-                return false;
-            }
-            $photo = $data['photo'];
-
-            $target_file = "../assets/Images/" . basename($_FILES["photo"]["name"]);  
-            move_uploaded_file($_FILES["photo"]["tmp_name"],__DIR__ . '/' . $target_file);
-            $photo = basename($_FILES["photo"]["name"]);
-
-            $data = [
-                'title' => $data['title'],
-                'body' => $data['body'],
-                'photo' => $photo,
-                'post_date' => $data['post_date']? $data['post_date']:  date('Y-m-d H:i:s') ,
-                'uid' => $data['uid']
-            ];
-            $this->save($data);
-
-        }catch(Exception $e) {
-            new Log($this->log_file, $e->getMessage());
-            return false;
-        }
-    }
-   public function save($data){
-        try{
-            $this->connect();
-
-            $title = $data['title'];
-            $body = $data['body'];
-            $photo = $data['photo'];
-            $post_date = $data['post_date'];
-            $uid = $data['uid'];
-            $table = 'articles';
-
-            $sql = "insert into `$table` (title, body, photo, post_date, uid) values ('$title', '$body', '$photo', '$post_date','$uid')";
-            if (mysqli_query($this->_dbHandler, $sql)) {
-                $id = mysqli_insert_id($this->_dbHandler);
-                $this->disconnect();
-                header('Location:/articles');
-                return $id;
-            } else {
-                $this->disconnect();
-                ob_flush();
-                header('location:/articles');
-                return false;
-            }
-        }catch(Exception $e){
-            new Log($this->log_file, $e->getMessage());
-            return false;
-        }
-    }
-
+  function buildInsertStatement($table, $data) {
+    $columns = implode(", ", array_keys($data));
+    $values = "'" . implode("', '", array_values($data)) . "'";
+    return "INSERT INTO $table ($columns) VALUES ($values)";
+}
     public function likeArticle($id) {
         $article = $this->getRecordByID($id)[0];
         $article_id = $article['aid'];
